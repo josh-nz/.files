c503267619a0d5bd2efae3f1ae39476eadece235
Joshua Wood <josh.nz@gmail.com>
2013-04-12 18:52:49 +1200

    Initial dotfiles commit.

---
+++ create-symlinks.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.files
+# Original starting point borrowed from:
+# https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+############################
+
+########## Variables
+
+dir=~/.files                    # dotfiles directory
+olddir=$dir/_dotfiles_old             # old dotfiles backup directory
+files="emacs.d"    # list of files/folders to symlink in homedir
+
+##########
+
+if [ -d $olddir ]; then
+  echo "Backups exist in $olddir. Sort these, delete $olddir and try again"
+  exit 1
+fi
+
+# move any existing dotfiles in homedir to backup directory, then create symlinks from the homedir to any files in the ~/.files directory specified in $files
+for file in $files; do
+    if [ -e ~/.$file ]; then
+        if [ -h ~/.$file ]; then
+          realdir=`readlink ~/.$file`
+          echo "Removing symlink ~/.$file which pointed to $realdir"
+          rm ~/.$file
+        else
+          if [ ! -d $olddir ]; then
+              mkdir $olddir
+          fi
+          echo "Backing up ~/.$file"
+          mv -f ~/.$file $olddir
+        fi
+    fi
+    echo "Creating symlink to .$file in home directory."
+    ln -s $dir/$file ~/.$file
+done
---
+++ emacs.d/init.el
@@ -0,0 +1,117 @@
+;;; info
+
+; C-u 0 M-x byte-recompile-directory
+; will compile all the .el files in the directory and in all subdirectories below.
+; The C-u 0 part is to make it not ask about every .el file that does
+; not have a .elc counterpart.
+
+; And thereafter (once each has been compiled once), just use the same
+; command without the C-u 0 (aka C-0, BTW). That will byte-compile
+; only those source files that are more recent than their
+; byte-compiled versions.
+
+;;; packaging
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+; There is a small bug in Emacs24’s package.el such that the
+; dependency order comes out backwards. The problem is patched by some
+; advice.
+
+(defadvice package-compute-transaction
+  (before package-compute-transaction-reverse (package-list requirements) activate compile)
+    "reverse the requirements"
+    (setq requirements (reverse requirements))
+    (print requirements))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+                      ;starter-kit-lisp
+                      ;starter-kit-bindings
+                      ;starter-kit-ruby
+                      ;starter-kit-js
+                      ;starter-kit-eshell
+                      ace-jump-mode
+                      auto-complete
+                      clojure-mode
+                      evil
+                      exec-path-from-shell
+                      yasnippet
+                      birds-of-paradise-plus-theme
+                      color-theme-sanityinc-tomorrow
+                      cyberpunk-theme
+                      django-theme
+                      gruber-darker-theme
+                      inkpot-theme
+                      pastels-on-dark-theme
+                      phoenix-dark-mono-theme
+                      phoenix-dark-pink-theme
+                      soothe-theme
+                      subatomic-theme
+                      tango-2-theme
+                      twilight-anti-bright-theme
+                      twilight-theme
+                      underwater-theme)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+ (when (not (package-installed-p p))
+  (package-install p)))
+
+;;; general settings
+(setq ring-bell-function 'ignore)
+(setq-default truncate-lines 0)
+(global-linum-mode 1)
+(setq create-lockfiles nil)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+; Use fn key as hyper. http://irreal.org/blog/?p=1450
+(setq ns-function-modifier 'hyper)
+
+; Copy env variables from shell on OSX
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(defadvice load-theme 
+  (before theme-dont-propagate activate)
+  (mapcar #'disable-theme custom-enabled-themes))
+(load-theme 'sanityinc-tomorrow-bright t)
+
+;;; ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+;(setq ibuffer-expert t)
+(add-hook 'ibuffer-mode-hook
+	  '(lambda ()
+	     (ibuffer-auto-mode 1)))
+(setq whitespace-style '(face tabs spaces trailing lines-tail newline indentation empty space-before-tab space-after-tab space-mark tab-mark newline-mark))
+
+;;; ido
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
+
+;;; emacs-starter-kit overrides
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+
+;;; evil mode
+(evil-mode 1)
+
+; esc quits
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+;;; autocomplete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20130330.1836/dict")
+(ac-config-default)
