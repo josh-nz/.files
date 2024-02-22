d93ec8d30a95dfb45f59db2d715db9bedb3d675a
Joshua Wood <josh.nz@gmail.com>
2024-02-22 18:25:15 +1300

    Nvim neo-tree shows "invisible" files.

--- nvim/.config/nvim/lua/plugins/neo_tree.lua
+++ nvim/.config/nvim/lua/plugins/neo_tree.lua
@@ -1,14 +1,22 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>t", ":Neotree filesystem reveal left<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        }
+      }
+    })
+
+    vim.keymap.set("n", "<leader>t", ":Neotree filesystem reveal left<CR>")
+  end,
 }
