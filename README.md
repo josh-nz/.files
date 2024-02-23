ee9151564ad3f0dba810ec163d660a0632d837ed
Joshua Wood <josh.nz@gmail.com>
2024-02-23 22:23:06 +1300

    Nvim make telescope show hidden files but not .git folder files.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-My dotfiles
-===========
+# My dotfiles
 
 This repo folder structure is such that it conforms to one expected by [Stow](https://www.gnu.org/software/stow/). An outline of how Stow works for dotfile usage is [here](https://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html).
 
@@ -20,8 +19,18 @@ stow */ # Everything (the '/' ignores this README)
 Unlink a folder with `stow -D <folder>`
 
 
-NeoVim config
-=============
+## NeoVim config
 
 A bunch of stuff was inspired or outright copied from [this repo](https://github.com/dmmulroy/kickstart.nix/tree/main/config/nvim) and as seen [in this video](https://www.youtube.com/watch?v=oo_I5lAmdi0).
+
+## Search
+
+The `search` folder contains a `.ignore` file.
+
+This file is used by tools such as [ripgrep](https://github.com/BurntSushi/ripgrep) and [fd](https://github.com/sharkdp/fd).
+
+The current `.ignore` contents allows the `--hidden` argument to be passed to ripgrep so that it will show hidden files (eg those starting with a `.`) while still ignoring the contents of a `.git` folder. This is how NeoVim Telescope is currently configured, for example.
+
+More specifics on the use of this file [here](https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#automatic-filtering).
+
 
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,74 +1,88 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 local telescope = {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-		},
-	},
-	config = function()
-		local actions = require("telescope.actions")
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build =
+      "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
+  },
+  config = function()
+    local actions = require("telescope.actions")
 
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-						["<C-x>"] = actions.delete_buffer,
-					},
-				},
-			},
-		})
-		
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-x>"] = actions.delete_buffer,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          -- Sets --hidden flag on ripgrep to show files starting with a `.`. 
+          -- As a result, this causes it to now find files in .git folders.
+          -- To prevent this, a .ignore file should be created that specifies
+          -- to ignore `.git/`
+          hidden = true,
+        }
+      },
+    })
 
-		require("telescope").load_extension("fzf")
-	end,
+    local builtin = require("telescope.builtin")
+    -- find_files calls `rg --files --colour` as priority, see link for details.
+    -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/builtin/__files.lua#L272
+    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+    -- :help telescope.defaults.vimgrep_arguments
+    -- will show the default arguments passed to ripgrep.
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    require("telescope").load_extension("fzf")
+  end,
 }
 
 -- https://github.com/nvim-telescope/telescope-ui-select.nvim
 local telescope_ui_select = {
-	"nvim-telescope/telescope-ui-select.nvim",
-	config = function()
-		require("telescope").setup({
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({
-						-- even more opts
-					}),
+  "nvim-telescope/telescope-ui-select.nvim",
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({
+            -- even more opts
+          }),
 
-					-- pseudo code / specification for writing custom displays, like the one
-					-- for "codeactions"
-					-- specific_opts = {
-					--   [kind] = {
-					--     make_indexed = function(items) -> indexed_items, width,
-					--     make_displayer = function(widths) -> displayer
-					--     make_display = function(displayer) -> function(e)
-					--     make_ordinal = function(e) -> string
-					--   },
-					--   -- for example to disable the custom builtin "codeactions" display
-					--      do the following
-					--   codeactions = false,
-					-- }
-				},
-			},
-		})
-		-- To get ui-select loaded and working with telescope, you need to call
-		-- load_extension, somewhere after setup function:
-		require("telescope").load_extension("ui-select")
-	end,
+          -- pseudo code / specification for writing custom displays, like the one
+          -- for "codeactions"
+          -- specific_opts = {
+          --   [kind] = {
+          --     make_indexed = function(items) -> indexed_items, width,
+          --     make_displayer = function(widths) -> displayer
+          --     make_display = function(displayer) -> function(e)
+          --     make_ordinal = function(e) -> string
+          --   },
+          --   -- for example to disable the custom builtin "codeactions" display
+          --      do the following
+          --   codeactions = false,
+          -- }
+        },
+      },
+    })
+    -- To get ui-select loaded and working with telescope, you need to call
+    -- load_extension, somewhere after setup function:
+    require("telescope").load_extension("ui-select")
+  end,
 }
 
 return {
-	telescope,
-	telescope_ui_select,
+  telescope,
+  telescope_ui_select,
 }
