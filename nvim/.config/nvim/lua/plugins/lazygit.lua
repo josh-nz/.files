35cf77ce8e7feca8284cb3785938d9f3ed934a49
Joshua Wood <josh.nz@gmail.com>
2024-03-03 15:13:30 +1300

    Nvim add LazyGit integration.

---
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+-- https://github.com/kdheepak/lazygit.nvim
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
