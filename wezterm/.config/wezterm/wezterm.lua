42be441cd31652f1e6892452dd151411512d5ce6
Joshua Wood <josh.nz@gmail.com>
2024-03-11 18:58:51 +1300

    WezTerm add custom page up/down key bindings.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -28,6 +28,12 @@ config.mouse_bindings = {
   },
 }
 
+local act = wezterm.action
+config.keys = {
+  { mods = 'CTRL', key = 'u', action = act.ScrollByPage(-1) },
+  { mods = 'CTRL', key = 'd', action = act.ScrollByPage(1) },
+}
+
 -- https://github.com/wez/wezterm/discussions/3733
 -- https://wezfurlong.org/wezterm/config/lua/gui-events/gui-startup.html
 -- https://wezfurlong.org/wezterm/config/lua/mux-events/mux-startup.html
