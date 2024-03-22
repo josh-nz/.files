3f80c67fc0a5a50d2f6a55351226d8bc556676b8
Joshua Wood <josh.nz@gmail.com>
2024-03-22 18:33:26 +1300

    Add lazygit config.

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,7 +1,7 @@
 # Not sure how safe these are to set on MacOS
 # set xdg paths
 # export XDG_CACHE_HOME="$HOME/.cache"
-# export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CONFIG_HOME="$HOME/.config"
 # export XDG_DATA_HOME="$HOME/.local/share"
 
 export EDITOR="nvim"
