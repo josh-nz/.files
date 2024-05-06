665f33db07f848aa618716cc9ed596ec4ffcf495
Joshua Wood <josh.nz@gmail.com>
2024-05-06 18:21:51 +1200

    Nvim add more colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/monet_nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/fynnfluegge/monet.nvim
+return {
+  "fynnfluegge/monet.nvim",
+  enabled = true,
+  lazy = false,
+  priority = 1000,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/okcolors_nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/e-q/okcolors.nvim
+return {
+  "e-q/okcolors.nvim",
+  enabled = true,
+  lazy = false,
+  priority = 1000,
+}
