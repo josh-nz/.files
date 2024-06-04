62c0c1244feb02bf62aed29fb7229386a67a7141
Joshua Wood <josh.nz@gmail.com>
2024-06-04 20:11:01 +1200

    Nvim disable auto-session plugin.

--- nvim/.config/nvim/lua/plugins/auto_session.lua
+++ nvim/.config/nvim/lua/plugins/auto_session.lua
@@ -1,7 +1,7 @@
 -- https://github.com/rmagatti/auto-session
 return {
   "rmagatti/auto-session",
-  enabled = true,
+  enabled = false,
   config = function()
 ---@diagnostic disable-next-line: missing-fields
     require("auto-session").setup({
