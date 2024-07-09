50daf080fc8e877aac84fd3094f77a9b92cfe43d
Joshua Wood <josh.nz@gmail.com>
2024-07-09 22:38:17 +1200

    Nvim add new colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/neomodern_nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/cdmill/neomodern.nvim
+return {
+  "cdmill/neomodern.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/oh_lucy_nvim.lua
@@ -0,0 +1,7 @@
+-- https://github.com/Yazeed1s/oh-lucy.nvim
+return {
+  "Yazeed1s/oh-lucy.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+}
