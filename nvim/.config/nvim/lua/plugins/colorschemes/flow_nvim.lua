bc5838f1e313c1c2020a0111cb020c323138beec
Joshua Wood <josh.nz@gmail.com>
2024-07-25 21:02:49 +1200

    Nvim fix flow colorscheme config.

--- nvim/.config/nvim/lua/plugins/colorschemes/flow_nvim.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes/flow_nvim.lua
@@ -4,9 +4,8 @@ return {
   enabled = true,
   lazy = true,
 	priority = 1000,
-  name = "flow",
   config = function()
-    require("flow").setup_options({
+    require("flow").setup({
       transparent = false, -- Set transparent background.
       fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
       mode = "normal", -- Intensity of the palette: normal, dark, or bright. Notice that dark is ugly!
