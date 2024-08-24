dba1a2925b4f48a3ed7d9626fbf2b9c777ca382c
Joshua Wood <josh.nz@gmail.com>
2024-08-25 11:12:32 +1200

    Nvim restore code to disable LSP semantic highlights.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,3 +36,10 @@ require("user.auto_commands")
 require("user.user_commands")
 
 require("user.wezterm_nvim_move")
+
+-- Hide all semantic highlights until upstream issues are resolved.
+-- https://github.com/catppuccin/nvim/issues/480
+-- for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+--   vim.api.nvim_set_hl(0, group, {})
+-- end
+
