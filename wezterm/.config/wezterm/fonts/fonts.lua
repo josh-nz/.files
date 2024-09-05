0bef6c81ce05e3d5e5e8f1780fb3af8c68543dbf
Joshua Wood <josh.nz@gmail.com>
2024-09-05 17:07:53 +1200

    WezTerm add note around why ligatures don't always render.

--- wezterm/.config/wezterm/fonts/fonts.lua
+++ wezterm/.config/wezterm/fonts/fonts.lua
@@ -31,7 +31,17 @@ M.activate = function(config, label, value)
   elseif label == "MonoLisa" then
     -- ss02 is the script variant.
     -- zero is slashed zero instead of dotted zero.
-    config.harfbuzz_features = { "ss02=1", "zero" }
+    -- liga set to zero disables ligatures.
+    -- Ligatures will not be displayed in WezTerm if their colour and
+    -- style etc are not consistent for all characters in the ligature.
+    -- This can happen for example with syntax highlighting or terminal
+    -- plugins. Other terminals, such as Kitty, don't seem to have this
+    -- limit, but instead the ligature is rendered as a mixture of the
+    -- different colours, styles, etc.
+    -- https://github.com/wez/wezterm/issues/2331
+    config.harfbuzz_features = { "ss02=1", "zero", } --"liga=0" }
+    -- Test ligatures:
+    -- != <!--
   else
     config.harfbuzz_features = {}
   end
