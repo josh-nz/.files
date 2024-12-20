f8000aabd96767226eccf2df5666ebf3533f6fd4
Joshua Wood <josh.nz@gmail.com>
2024-12-20 19:13:53 +1300

    Nvim add bafa open buffer navigation plugin.

---
+++ nvim/.config/nvim/lua/plugins/bafa.lua
@@ -0,0 +1,15 @@
+-- https://github.com/mistweaverco/bafa.nvim
+return {
+  "mistweaverco/bafa.nvim",
+  enabled = false,
+  opts = {
+    width = 60,
+    height = 10,
+    title = "Bafa",
+    title_pos = "center",
+    relative = "editor",
+    border = "rounded",
+    style = "minimal",
+    diagnostics = true,
+  }
+}
