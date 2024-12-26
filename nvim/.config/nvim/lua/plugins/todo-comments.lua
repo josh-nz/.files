c04ccf489a8879a9080360a51364a5a40a772bcd
Joshua Wood <josh.nz@gmail.com>
2024-12-27 12:46:54 +1300

    Nvim adjust todo comment plugin to support name reference.

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -7,6 +7,12 @@ return {
   opts = {
     highlight = {
       multiline = false,
+      -- vimgrep regex, supporting the pattern TODO(name):
+      pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
     },
+    search = {
+      -- ripgrep regex, supporting the pattern TODO(name):
+      pattern = [[\b(KEYWORDS)(\(\w*\))*:]],
+    }
   },
 }
