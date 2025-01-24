95b10bbb44c960782cf6611f7e1f8f09e3a77a36
Joshua Wood <josh.nz@gmail.com>
2025-01-24 19:16:11 +1300

    Nvim remove no longer needed lazydev.nvim dependency.

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -6,14 +6,11 @@ return {
     "folke/lazydev.nvim",
     enabled = true,
     ft = "lua", -- only load on lua files
-    dependencies = {
-      { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-    },
     opts = {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
