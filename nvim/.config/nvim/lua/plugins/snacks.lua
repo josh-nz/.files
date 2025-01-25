0a7cb736df0111983b76aadf6239760279fd8ed6
Joshua Wood <josh.nz@gmail.com>
2025-01-25 20:51:25 +1300

    Nvim add default snacks picker keymaps for reference.

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,121 @@
 -- https://github.com/folke/snacks.nvim
+
+
+-- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md
+local picker_config = {
+  enabled = true,
+  win = {
+    -- input window
+    input = {
+      keys = {
+        ["<Esc>"] = "close",
+        ["<C-c>"] = { "close", mode = "i" },
+        -- to close the picker on ESC instead of going to normal mode,
+        -- add the following keymap to your config
+        -- ["<Esc>"] = { "close", mode = { "n", "i" } },
+        ["<CR>"] = { "confirm", mode = { "n", "i" } },
+        ["G"] = "list_bottom",
+        ["gg"] = "list_top",
+        ["j"] = "list_down",
+        ["k"] = "list_up",
+        ["/"] = "toggle_focus",
+        ["q"] = "close",
+        ["?"] = "toggle_help",
+        ["<a-d>"] = { "inspect", mode = { "n", "i" } },
+        ["<c-a>"] = { "select_all", mode = { "n", "i" } },
+        ["<a-m>"] = { "toggle_maximize", mode = { "i", "n" } },
+        ["<a-p>"] = { "toggle_preview", mode = { "i", "n" } },
+        ["<a-w>"] = { "cycle_win", mode = { "i", "n" } },
+        ["<C-w>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
+        ["<C-Up>"] = { "history_back", mode = { "i", "n" } },
+        ["<C-Down>"] = { "history_forward", mode = { "i", "n" } },
+        ["<Tab>"] = { "select_and_next", mode = { "i", "n" } },
+        ["<S-Tab>"] = { "select_and_prev", mode = { "i", "n" } },
+        ["<Down>"] = { "list_down", mode = { "i", "n" } },
+        ["<Up>"] = { "list_up", mode = { "i", "n" } },
+        ["<c-j>"] = { "list_down", mode = { "i", "n" } },
+        ["<c-k>"] = { "list_up", mode = { "i", "n" } },
+        ["<c-n>"] = { "list_down", mode = { "i", "n" } },
+        ["<c-p>"] = { "list_up", mode = { "i", "n" } },
+        ["<c-l>"] = { "preview_scroll_left", mode = { "i", "n" } },
+        ["<c-h>"] = { "preview_scroll_right", mode = { "i", "n" } },
+        ["<c-b>"] = { "preview_scroll_up", mode = { "i", "n" } },
+        ["<c-d>"] = { "list_scroll_down", mode = { "i", "n" } },
+        ["<c-f>"] = { "preview_scroll_down", mode = { "i", "n" } },
+        ["<c-g>"] = { "toggle_live", mode = { "i", "n" } },
+        ["<c-u>"] = { "list_scroll_up", mode = { "i", "n" } },
+        ["<ScrollWheelDown>"] = { "list_scroll_wheel_down", mode = { "i", "n" } },
+        ["<ScrollWheelUp>"] = { "list_scroll_wheel_up", mode = { "i", "n" } },
+        ["<c-v>"] = { "edit_vsplit", mode = { "i", "n" } },
+        ["<c-s>"] = { "edit_split", mode = { "i", "n" } },
+        ["<c-q>"] = { "qflist", mode = { "i", "n" } },
+        ["<a-i>"] = { "toggle_ignored", mode = { "i", "n" } },
+        ["<a-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+        ["<a-f>"] = { "toggle_follow", mode = { "i", "n" } },
+      },
+    },
+    -- result list window
+    list = {
+      keys = {
+        ["<CR>"] = "confirm",
+        ["gg"] = "list_top",
+        ["G"] = "list_bottom",
+        ["i"] = "focus_input",
+        ["j"] = "list_down",
+        ["k"] = "list_up",
+        ["q"] = "close",
+        ["<Tab>"] = "select_and_next",
+        ["<S-Tab>"] = "select_and_prev",
+        ["<Down>"] = "list_down",
+        ["<Up>"] = "list_up",
+        ["<a-d>"] = "inspect",
+        ["<c-d>"] = "list_scroll_down",
+        ["<c-u>"] = "list_scroll_up",
+        ["zt"] = "list_scroll_top",
+        ["zb"] = "list_scroll_bottom",
+        ["zz"] = "list_scroll_center",
+        ["/"] = "toggle_focus",
+        ["<ScrollWheelDown>"] = "list_scroll_wheel_down",
+        ["<ScrollWheelUp>"] = "list_scroll_wheel_up",
+        ["<c-a>"] = "select_all",
+        ["<c-f>"] = "preview_scroll_down",
+        ["<c-b>"] = "preview_scroll_up",
+        ["<c-l>"] = "preview_scroll_right",
+        ["<c-h>"] = "preview_scroll_left",
+        ["<c-v>"] = "edit_vsplit",
+        ["<c-s>"] = "edit_split",
+        ["<c-j>"] = "list_down",
+        ["<c-k>"] = "list_up",
+        ["<c-n>"] = "list_down",
+        ["<c-p>"] = "list_up",
+        ["<a-w>"] = "cycle_win",
+        ["<Esc>"] = "close",
+      },
+    },
+    -- preview window
+    preview = {
+      keys = {
+        ["<Esc>"] = "close",
+        ["q"] = "close",
+        ["i"] = "focus_input",
+        ["<ScrollWheelDown>"] = "list_scroll_wheel_down",
+        ["<ScrollWheelUp>"] = "list_scroll_wheel_up",
+        ["<a-w>"] = "cycle_win",
+      },
+    },
+  },
+}
+
+
+
+
+
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  import = "plugins.snacks",
   opts = {
     -- Deal with big files
     bigfile = { enabled = true },
@@ -32,7 +145,7 @@ return {
     -- Pretty vim.notify
     notifier = { enabled = false },
     -- Picker for selecting items
-    picker = { enabled = true },
+    picker = picker_config,
     -- Neovim lua profiler
     profiler = { enabled = false },
     -- When doing nvim somefile.txt, it will render the file as quickly as possible, before loading your plugins.
@@ -59,7 +172,7 @@ return {
     zen = { enabled = false },
     styles = {
       lazygit = {
-        -- zero is full width/height.
+        -- Zero is full width/height
         height = 0,
         width = 0,
       },
