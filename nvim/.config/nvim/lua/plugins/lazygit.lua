07e094f2e95494e8dd20f14ca2a227440aab2dbe
Joshua Wood <josh.nz@gmail.com>
2025-01-25 16:39:40 +1300

    Nvim disable lazygit plugin, using snack's version.

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -1,7 +1,7 @@
 -- https://github.com/kdheepak/lazygit.nvim
 return {
   "kdheepak/lazygit.nvim",
-  enabled = true,
+  enabled = false,
   -- optional for floating window border decoration
   dependencies = {
     "nvim-lua/plenary.nvim",
