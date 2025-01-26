475a40785565940348fb581592d6af0c4e2024ca
Joshua Wood <josh.nz@gmail.com>
2025-01-26 20:54:11 +1300

    Nvim add two more colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/monokai_pro.lua
@@ -0,0 +1,8 @@
+-- https://github.com/loctvl842/monokai-pro.nvim
+return {
+  "loctvl842/monokai-pro.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  config = true,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/white_chocolate.lua
@@ -0,0 +1,8 @@
+-- https://github.com/EtiamNullam/white-chocolate.nvim
+return {
+  "EtiamNullam/white-chocolate.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  config = true,
+}
