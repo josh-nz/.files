a7f2d166f8105a5e9616b774628ae3873b9ace05
Joshua Wood <josh.nz@gmail.com>
2025-02-10 20:10:13 +1300

    Nvim add two new colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/embark.lua
@@ -0,0 +1,8 @@
+-- https://github.com/embark-theme/vim
+return {
+  "embark-theme/vim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  name = "embark",
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/farout.lua
@@ -0,0 +1,8 @@
+-- https://github.com/thallada/farout.nvim
+return {
+  "thallada/farout.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  opts = {},
+}
