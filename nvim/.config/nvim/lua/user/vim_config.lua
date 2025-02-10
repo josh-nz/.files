903387997b4626a2337239d48d6fec51c9e13134
Joshua Wood <josh.nz@gmail.com>
2025-02-10 20:15:07 +1300

    Nvim add diagnostic colors to numberline.

--- nvim/.config/nvim/lua/user/vim_config.lua
+++ nvim/.config/nvim/lua/user/vim_config.lua
@@ -137,6 +137,21 @@ vim.diagnostic.config({
   float = {
     border = "rounded",
   },
+  signs = {
+    -- text = {
+    --   [vim.diagnostic.severity.ERROR] = '',
+    --   [vim.diagnostic.severity.WARN] = '',
+    --   [vim.diagnostic.severity.INFO] = '',
+    --   [vim.diagnostic.severity.HINT] = '',
+    -- },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+
+    },
+  },
 })
 
 
