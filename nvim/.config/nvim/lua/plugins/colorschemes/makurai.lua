cde9cc27010d0e4905a66b47c79aa5548a341a01
Joshua Wood <josh.nz@gmail.com>
2025-03-28 18:48:49 +1300

    Nvim add two colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/makurai.lua
@@ -0,0 +1,7 @@
+-- https://github.com/Skardyy/makurai-nvim
+return {
+  "Skardyy/makurai-nvim",
+  enabled = true,
+  lazy = false,
+	priority = 1000,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/oscura.lua
@@ -0,0 +1,7 @@
+-- https://github.com/vinitkumar/oscura-vim
+return {
+  "vinitkumar/oscura-vim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+}
