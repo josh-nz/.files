ecc04b7b3d47e6e383e1b8d695c65d71ddf60851
Joshua Wood <josh.nz@gmail.com>
2025-06-13 19:40:57 +1200

    Nvim add simple word toggle function example.

--- nvim/.config/nvim/lua/user/utils.lua
+++ nvim/.config/nvim/lua/user/utils.lua
@@ -10,4 +10,26 @@ M.is_git_directory = function()
 	end
 end
 
+-- https://www.reddit.com/r/neovim/comments/1l9l4q0/i_cannot_live_without_this_plugin/
+M.toggle_word = function()
+	local toggles = {
+		["true"] = "false",
+		["always"] = "never",
+		["yes"] = "no",
+		-- ...
+	}
+
+	local cword = vim.fn.expand("<cword>")
+	local newWord
+	for word, opposite in pairs(toggles) do
+		if cword == word then newWord = opposite end
+		if cword == opposite then newWord = word end
+	end
+	if newWord then
+		local prevCursor = vim.api.nvim_win_get_cursor(0)
+		vim.cmd.normal { '"_ciw' .. newWord, bang = true }
+		vim.api.nvim_win_set_cursor(0, prevCursor)
+	end
+end
+
 return M
