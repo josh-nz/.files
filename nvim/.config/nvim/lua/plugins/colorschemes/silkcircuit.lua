cbd6172fe163ab919bc4ab6d708ba85ccd67da38
Joshua Wood <josh.nz@gmail.com>
2025-08-28 18:10:00 +1200

    Nvim add silkcircuit and techbase colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/silkcircuit.lua
@@ -0,0 +1,7 @@
+-- https://github.com/hyperb1iss/silkcircuit-nvim
+return {
+  "hyperb1iss/silkcircuit-nvim",
+  enabled = true,
+  lazy = false,
+  priority = 1000,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/techbase.lua
@@ -0,0 +1,7 @@
+-- https://github.com/mcauley-penney/techbase.nvim
+return {
+  "mcauley-penney/techbase.nvim",
+  enabled = false,
+  lazy = true,
+  priority = 1000,
+}
