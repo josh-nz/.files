eff03e2127410a20035770b92b11dbaa0669e6a1
Joshua Wood <josh.nz@gmail.com>
2025-11-23 21:18:31 +1300

    Nvim add highlight_colors plugin.

---
+++ nvim/.config/nvim/lua/plugins/highlight_colors.lua
@@ -0,0 +1,10 @@
+-- https://github.com/brenoprata10/nvim-highlight-colors
+return {
+  "brenoprata10/nvim-highlight-colors",
+  enabled = true,
+  event = "VeryLazy",
+  opts = {
+    -- render = "foreground",
+    -- render = "virtual",
+  },
+}
