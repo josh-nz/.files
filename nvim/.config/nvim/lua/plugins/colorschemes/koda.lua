1a5d0be1fc986d1cd8513d854a21f044794a08fd
Joshua Wood <josh.nz@gmail.com>
2026-01-12 18:41:21 +1300

    Nvim add koda, teide, and wisteria colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/koda.lua
@@ -0,0 +1,12 @@
+-- https://github.com/oskarnurm/koda.nvim
+return {
+  "oskarnurm/koda.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  opts = {
+    styles = {
+      comments = { italic = true },
+    },
+  },
+}
