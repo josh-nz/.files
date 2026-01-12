336012d21ae889346e0fecabe11be28e8bdce34b
Joshua Wood <josh.nz@gmail.com>
2026-01-12 18:41:21 +1300

    Nvim add koda, teide, and wisteria colorschemes.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/teide.lua
@@ -0,0 +1,8 @@
+-- https://github.com/serhez/teide.nvim
+return {
+  "serhez/teide.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  opts = {},
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/wisteria.lua
@@ -0,0 +1,7 @@
+-- https://github.com/masisz/wisteria.nvim
+return {
+  "masisz/wisteria.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+}
