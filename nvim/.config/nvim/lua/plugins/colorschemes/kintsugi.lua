e31140eaa8fc84b89de4593c78becaaa5894b61e
Joshua Wood <josh.nz@gmail.com>
2026-04-30 18:03:03 +1200

    Nvim add kintsugi colorscheme.

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/kintsugi.lua
@@ -0,0 +1,10 @@
+-- https://github.com/metalelf0/kintsugi-nvim
+return {
+  "metalelf0/kintsugi-nvim",
+  enabled = true,
+  lazy = true,
+	priority = 1000,
+  opts = {
+    italic_comments = true,
+  }
+}
