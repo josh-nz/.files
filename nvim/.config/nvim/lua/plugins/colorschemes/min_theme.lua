25d13abaf19308c8e982fc993e6098b3d5a1fe3e
Joshua Wood <josh.nz@gmail.com>
2026-06-29 15:52:09 +1200

    Nvim add opencode colorscheme, remove min_theme.

--- nvim/.config/nvim/lua/plugins/colorschemes/min_theme.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/datsfilipe/min-theme.nvim
-return {
-  "datsfilipe/min-theme.nvim",
-  enabled = false,
-  lazy = true,
-  priority = 1000,
-}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/opencode.lua
@@ -0,0 +1,7 @@
+-- https://github.com/builtbyleo/oc-2.nvim
+return {
+  "builtbyleo/oc-2.nvim",
+  enabled = false,
+  lazy = true,
+	priority = 1000,
+}
